8a22a58df05a551e4033d32dfd9e492a1f05e9e4
Eboher <940084565@qq.com>
2019-09-23 17:49:16 +0800

    加上了kafka 👀

--- src/main/java/com/magnetom/loki/LokiApplication.java
+++ src/main/java/com/magnetom/loki/LokiApplication.java
@@ -1,13 +1,24 @@
 package com.magnetom.loki;
 
+import com.magnetom.loki.kafka.KafkaSender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LokiApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(LokiApplication.class, args);
-    }
 
+        ConfigurableApplicationContext context = SpringApplication.run(LokiApplication.class, args);
+        KafkaSender sender = context.getBean(KafkaSender.class);
+        for (int i = 0; i < 3; i++) {
+            sender.send();
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
